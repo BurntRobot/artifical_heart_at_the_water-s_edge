e6bbc232fac880fe55c7525a09d8dd74c0e08acc
BurntRobot <Bydan14@yandex.ru>
2025-11-06 16:30:40 +0300

    Trying to make a camera movement

--- Player/player_camera.gd
+++ Player/player_camera.gd
@@ -1,5 +1,22 @@
 extends Camera2D
 
+@export var SCREEN_BORDER_FOR_MOVEMENT = 20
+
+
+func _ready() -> void:
+	position.x = (limit_right - limit_left) / 2
+	position.y = (limit_bottom - limit_top) / 2
+
+func _physics_process(delta: float) -> void:
+	if get_global_mouse_position().x <= 0 + SCREEN_BORDER_FOR_MOVEMENT:
+		print(position.x)
+		#position.x -= 1
+		#print(event.relative)
 
 func _input(event: InputEvent) -> void:
-	pass
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			zoom += Vector2(0.2, 0.2)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			zoom -= Vector2(0.2, 0.2)
+	
