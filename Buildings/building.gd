484f1ed8b21b8041ce2e971bd248350337f55ecd
BurntRobot <Bydan14@yandex.ru>
2025-11-09 22:28:23 +0300

    Added resources and building cost

--- Buildings/building.gd
+++ Buildings/building.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var base: TileMapLayer = $Base
 @onready var sprite: Sprite2D = $Sprite
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var second_timer: Timer = $SecondTimer
+@onready var minute_timer: Timer = $MinuteTimer
+
+var building_cost: int = 400
 
 var rotated: bool = false
 var is_built: bool = false
@@ -11,13 +15,20 @@ var is_built: bool = false
 func _process(_delta: float) -> void:
 	if not is_built:
 		position = get_global_mouse_position().snapped(Vector2(30, 18))
+		if CityResources.money - building_cost < 0:
+			sprite.self_modulate = Color('ff000089')
+		else:
+			sprite.self_modulate = Color('ffffff89')
 
 func _input(event: InputEvent) -> void:
 	if not is_built:
 		if event.is_action_pressed("rotate"):
 			_rotate()
 		if event.is_action_pressed("build"):
-			_build()
+			if CityResources.money - building_cost >= 0:
+				_build()
+			else:
+				animation_player.play("cant_build")
 
 func _rotate():
 	base.rotate_base(rotated)
@@ -27,3 +38,12 @@ func _rotate():
 func _build():
 	animation_player.play("start_building")
 	is_built = true
+	second_timer.start()
+	minute_timer.start()
+	CityResources.money -= building_cost
+
+func _on_second_timer_timeout() -> void:
+	pass # Replace with function body.
+
+func _on_minute_timer_timeout() -> void:
+	pass # Replace with function body.
---
+++ city_resources.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var money:int = 1000
+
+var water:int = 100
+var life_water:int = 30
+var golder_ore:int = 0
+
+var citizens:int = 0
+var workers:int = 0
+var unemployed:int = 0
