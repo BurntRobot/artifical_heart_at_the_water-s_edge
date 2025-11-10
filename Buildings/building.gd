61db4a1723e4e938a569484d481a4898a88e4c74
BurntRobot <Bydan14@yandex.ru>
2025-11-10 15:15:32 +0300

    Trying to make right placing algorithm

--- Buildings/building.gd
+++ Buildings/building.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Building
 
 @onready var base: TileMapLayer = $Base
+@onready var base_for_water: TileMapLayer = $BaseForWater
 @onready var sprite: Sprite2D = $Sprite
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -9,28 +10,38 @@ class_name Building
 
 var rotated: bool = false
 var is_built: bool = false
+var can_build: bool = true
+
+signal _on_check_free_ground(_tile_map: Array[Vector2i])
+signal _on_check_free_water(_tile_map: Array[Vector2i])
 
 
 func _ready() -> void:
 	CityResources._on_next_day.connect(_next_day)
 	CityResources._on_next_month.connect(_next_month)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _physics_process(_delta: float) -> void:
 	if not is_built:
-		position = get_global_mouse_position().snapped(Vector2(30, 18))
+		if position != get_global_mouse_position().snapped(Vector2(30, 18)):
+			position = get_global_mouse_position().snapped(Vector2(30, 18))
 		if CityResources.money - building_cost < 0:
 			sprite.self_modulate = Color('ff000089')
 			base._set_all_tiles_to_red()
 		else:
 			sprite.self_modulate = Color('ffffff89')
 			base._update_tiles_colors()
+			can_build = true
+		_on_check_free_ground.emit(base.get_used_cells())
+		if base_for_water.get_used_cells() == null:
+			_on_check_free_water.emit(base_for_water.get_used_cells())
 
 func _input(event: InputEvent) -> void:
 	if not is_built:
 		if event.is_action_pressed("rotate"):
 			_rotate()
 		if event.is_action_pressed("build"):
-			if CityResources.money - building_cost >= 0:
+			if CityResources.money - building_cost >= 0 and can_build:
 				_build()
 			else:
 				animation_player.play("cant_build")
@@ -44,6 +55,12 @@ func _build():
 	animation_player.play("start_building")
 	is_built = true
 	CityResources.money -= building_cost
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _next_day(): pass
 func _next_month(): pass
+
+func set_red_places(_red_tiles: Array[Vector2i]):
+	can_build = false
+	for _tile in _red_tiles:
+		base.set_cell(_tile, 0, Vector2(3, 0))
--- Map/map.gd
+++ Map/map.gd
@@ -2,7 +2,41 @@ extends Node2D
 
 @export var buildings_scenes: Array[PackedScene]
 
+@onready var buildings_map: TileMapLayer = $BuildingsMap
+@onready var base_map: TileMapLayer = $BaseMap
+
 
 func _on_test_build_pressed() -> void:
-	var church = buildings_scenes[0].instantiate()
-	add_child(church)
+	var town_hall = buildings_scenes[0].instantiate()
+	town_hall._on_check_free_ground.connect(is_tiles_are_free_ground)
+	add_child(town_hall)
+
+func is_tiles_are_free_ground(tiles: Array[Vector2i]):
+	for child in get_children():
+		if child is Building and not child.is_built:
+			var _red_tiles: Array[Vector2i] = []
+			var base_for_tile = Vector2i(
+				int(child.position.x / 30),
+				int(child.position.y / 18))
+			for _tile in tiles:
+				if is_tile_a_ground(_tile + base_for_tile):
+					_red_tiles.append(_tile)
+			child.set_red_places(_red_tiles)
+
+func is_tiles_are_free_water(tiles):
+	pass
+
+func is_tile_a_ground(_tile: Vector2i):
+	return _tile in get_ground_tiles()
+func get_ground_tiles():
+	return base_map.get_used_cells_by_id(0, Vector2i(0, 0))
+
+func is_tile_a_water():
+	pass
+func get_water_tiles():
+	return base_map.get_used_cells_by_id(0, Vector2i(1, 0))
+
+func is_tile_builded():
+	pass
+func get_builded_tiles():
+	return buildings_map.get_used_cells_by_id(0, Vector2i(2, 0))
