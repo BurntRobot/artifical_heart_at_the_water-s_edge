30dc4cc1e33617744513e25b5baab7a6cb2a9879
BurntRobot <Bydan14@yandex.ru>
2025-11-25 23:36:55 +0300

    Set lines for debug

--- Player/camera_pivot.gd
+++ Player/camera_pivot.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 @onready var player_camera: Camera2D = $PlayerCamera
+
+var LEFT_MOUSE_BORDER
+var RIGHT_MOUSE_BORDER
+var UP_MOUSE_BORDER
+var DOWN_MOUSE_BORDER
+var border_width: int = 10
+
 var LEFT_BORDER
 var RIGHT_BORDER
 var UP_BORDER
 var DOWN_BORDER
 var camera_speed: int = 150
+
 
 
 func _ready() -> void:
@@ -16,26 +24,36 @@ func _process(delta: float) -> void:
 	_set_borders()
 	if Input.is_action_pressed("left") and position.x > LEFT_BORDER:
 		position += Vector2.LEFT * camera_speed * delta
-		print("Left")
 	if Input.is_action_pressed("right") and position.x < RIGHT_BORDER:
 		position += Vector2.RIGHT * camera_speed * delta
-		print("Right")
 	if Input.is_action_pressed("up") and position.y > UP_BORDER:
 		position += Vector2.UP * camera_speed * delta
-		print("Up")
 	if Input.is_action_pressed("down") and position.y < DOWN_BORDER:
 		position += Vector2.DOWN * camera_speed * delta
-		print("Down")
 
 func _input(_event: InputEvent) -> void:
 	pass
 
 func _set_borders():
-	LEFT_BORDER = player_camera.limit_left + get_viewport_rect().size.x / 2
-	RIGHT_BORDER = player_camera.limit_right - get_viewport_rect().size.x / 2
-	UP_BORDER = player_camera.limit_top + get_viewport_rect().size.y / 2
-	DOWN_BORDER = player_camera.limit_bottom - get_viewport_rect().size.y / 2
-
+	var half_screen_x = get_viewport_rect().size.x / 2
+	var half_screen_y = get_viewport_rect().size.y / 2
+	var mouse_border_x = get_viewport_rect().size.x / border_width
+	var mouse_border_y = get_viewport_rect().size.y / border_width
+	
+	LEFT_BORDER = player_camera.limit_left + half_screen_x
+	RIGHT_BORDER = player_camera.limit_right - half_screen_x
+	UP_BORDER = player_camera.limit_top + half_screen_y
+	DOWN_BORDER = player_camera.limit_bottom - half_screen_y
+	
+	LEFT_MOUSE_BORDER = -half_screen_x + mouse_border_x
+	RIGHT_MOUSE_BORDER = half_screen_x - mouse_border_x
+	UP_MOUSE_BORDER = -half_screen_y + mouse_border_y
+	DOWN_MOUSE_BORDER = half_screen_y - mouse_border_y
+	
+	$LeftLine2D.position = Vector2(LEFT_MOUSE_BORDER, -half_screen_y)
+	$RightLine2D.position = Vector2(RIGHT_MOUSE_BORDER, -half_screen_y)
+	$UpLine2D.position = Vector2(-half_screen_x, UP_MOUSE_BORDER)
+	$DownLine2D.position = Vector2(-half_screen_x, DOWN_MOUSE_BORDER)
 
 
 """
