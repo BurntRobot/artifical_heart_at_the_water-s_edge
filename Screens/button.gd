4cb66d5c2204a5c35a79f437db8cef7b247cda26
BurntRobot <Bydan14@yandex.ru>
2025-11-28 10:22:11 +0300

    Small change and export preset

--- Screens/button.gd
+++ Screens/button.gd
@@ -1,6 +1,8 @@
 extends Button
 
+@onready var new_game = preload("res://world.tscn")
+
 
 func _on_pressed() -> void:
 	CityResources.restart_game()
-	get_tree().change_scene_to_file("res://World.tscn")
+	get_tree().change_scene_to_packed(new_game)
